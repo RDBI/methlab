2dcab64d0345c753e0b522160241fc4caffb9cd4
Erik Hollensbe <erik@hollensbe.org>
2010-04-23 17:56:21 -0400

    * Picky Pistos wants me to rename stuff.

--- test/test_checks.rb
+++ test/test_checks.rb
@@ -9,7 +9,7 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'test/unit'
 require 'methlab'
 
-class CheckClass
+class CheckedClass
     extend MethLab
 
     named_method(:named, :stuff => String, :stuff2 => [ /pee/, :required ], :stuff3 => :required) do |params| 
@@ -21,7 +21,7 @@ class CheckClass
     end
 end
 
-module CheckModule
+module CheckedModule
     extend MethLab
 
     named_method(:named, :stuff => String, :stuff2 => [ /pee/, :required ], :stuff3 => :required) do |params| 
@@ -35,58 +35,58 @@ end
 
 class TestChecks < Test::Unit::TestCase
     def setup
-        @check = CheckClass.new
+        @checked = CheckedClass.new
     end
 
     def test_01_named
-        assert(@check.respond_to?(:named))
+        assert(@checked.respond_to?(:named))
         
         assert_raises(ArgumentError.new("value of argument 'stuff' is an invalid type. Requires 'String'")) do
-            @check.named(:stuff => 1, :stuff2 => "pee", :stuff3 => 1)
+            @checked.named(:stuff => 1, :stuff2 => "pee", :stuff3 => 1)
         end
         
         assert_raises(ArgumentError.new("value of argument 'stuff2' does not match this regexp: '(?-mix:pee)'")) do
-            @check.named(:stuff => "foo", :stuff2 => "bar", :stuff3 => 1)
+            @checked.named(:stuff => "foo", :stuff2 => "bar", :stuff3 => 1)
         end
 
         assert_raises(ArgumentError.new("argument(s) 'stuff2' were not found but are required by the prototype")) do
-            @check.named(:stuff => "foo", :stuff3 => 1)
+            @checked.named(:stuff => "foo", :stuff3 => 1)
         end
 
         assert_raises(ArgumentError.new("argument(s) 'stuff2, stuff3' were not found but are required by the prototype")) do
-            @check.named(:stuff => "foo")
+            @checked.named(:stuff => "foo")
         end
 
         assert_raises(ArgumentError.new("argument(s) 'stuff2' were not found but are required by the prototype")) do
-            @check.named(:stuff => "foo", :stuff3 => nil)
+            @checked.named(:stuff => "foo", :stuff3 => nil)
         end
 
         assert_equal(
-            @check.named(:stuff => "foo", :stuff2 => "poopee", :stuff3 => 1), 
+            @checked.named(:stuff => "foo", :stuff2 => "poopee", :stuff3 => 1), 
             ["foo", "poopee", 1]
         )
     end
 
     def test_02_checked
-        assert(@check.respond_to?(:sequential))
+        assert(@checked.respond_to?(:sequential))
 
         assert_raises(ArgumentError.new("value of argument '0' is an invalid type. Requires 'String'")) do
-            @check.sequential(nil)
+            @checked.sequential(nil)
         end
         
         assert_raises(ArgumentError.new("value of argument '1' is an invalid type. Requires 'Integer'")) do
-            @check.sequential("foo", "bar")
+            @checked.sequential("foo", "bar")
         end
         
         assert_raises(ArgumentError.new("value of argument '1' is an invalid type. Requires 'Integer'")) do
-            @check.sequential("foo", nil)
+            @checked.sequential("foo", nil)
         end
 
         assert_raises(ArgumentError.new("too many arguments (3 for 2)")) do
-            @check.sequential("foo", 1, nil)
+            @checked.sequential("foo", 1, nil)
         end
 
-        assert_equal(@check.sequential("foo"), ["foo"])
-        assert_equal(@check.sequential("foo", 1), ["foo", 1])
+        assert_equal(@checked.sequential("foo"), ["foo"])
+        assert_equal(@checked.sequential("foo", 1), ["foo", 1])
     end
 end
